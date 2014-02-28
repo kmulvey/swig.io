eef4d088d42fbc546b147656c04f71059ba1b508
Kevin Mulvey <kmulvey@shutterstock.com>
2014-02-28 12:55:31 -0500

    submission

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 	$('form').on('submit', function(e){
 		e.preventDefault();
-		$(this).find('input[name=email], button').hide();
+		$(this).find('input, button').hide();
 		$('#api_key').val('api key val');
 		$('#api').show();
+		/*
 		$.post('http://swig.io/new', $(this).serialize(), function(d,t,x){
 			console.log(d);
 			console.log(t);
 		}).fail(function() {
 			$(this).find('label.error').show();
 		});
+		*/
 	});
 }); 
